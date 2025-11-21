ebec18364c5e9989b822868d6a91aee9e2a7f032
ayyubzada <ayyubzada@gmail.com>
2025-11-21 22:27:03 +0100

    initial

---
+++ Axin.Api/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Axin.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+
+    }
+}
